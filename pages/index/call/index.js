c723db898d9a70616f1beb65cfd11adf426a1283
freedomgzh <519668307@qq.com>
2019-01-17 17:29:19 +0800

    11

--- pages/index/call/index.js
+++ pages/index/call/index.js
@@ -15,9 +15,33 @@ Page({
    */
   onLoad: function (options) {
     var id = options.id
+    var that =this;
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) { } else {
+          that.setData({
+            hasUserInfo: false
+          })
+        }
+      }
+    })
 
-    this.getCode(id)
-    app.login()
+    if (app.globalData.userInfo) {
+      // 不显示权限
+
+      // that.getIndexList();
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      console.log("没有userinfo，callback赋值");
+      app.userInfoReadyCallback = (res, res1) => {
+        console.log("getuserinfo，获取用户信息", res, "res1", res1);
+        app.userLogin(res.userInfo.nickName, res.userInfo.avatarUrl, res1.code, function () {
+          console.log(2222)
+          that.getCode(id)
+        });
+      }
+    }
   },
   getCode: function (id) {
     var that = this
@@ -56,6 +80,26 @@ Page({
       }
     })
 
+  },
+  getUserInfo: function (e) {
+    console.log("e", e)
+    if (e.detail.userInfo) {
+      console.log("授权成功");
+      app.globalData.userInfo = e.detail.userInfo;
+      wx.setStorageSync("userInfo", app.globalData.userInfo)
+
+      // app.userLogin(e.detail.userInfo.nickName, e.detail.userInfo.avatarUrl, e.detail.code);
+      app.login()
+      wx.showLoading({
+        title: '加载中',
+      })
+      wx.hideLoading()
+      this.setData({
+        hasUserInfo: true
+      })
+    } else {
+      console.log("授权失败");
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
